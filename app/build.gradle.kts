a2a9c308dbf1d5e9829c0e6745f09b0e0b051041
ajain8616 <ajain8616@gmail.com>
2025-03-28 22:57:42 +0530

    update ui and update the code with the bottom_sheet and implement the adapters and add the filtered Activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,19 +43,19 @@ android {
 dependencies {
 
     implementation ("com.github.AnyChart:AnyChart-Android:1.1.5")
-    implementation ("com.google.firebase:firebase-auth:23.0.0")
-    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
-    implementation("com.google.android.gms:play-services-auth:21.1.1")
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation ("com.google.firebase:firebase-auth:23.2.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.11.0"))
+    implementation("com.google.android.gms:play-services-auth:21.3.0")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("com.google.firebase:firebase-auth:23.2.0")
     implementation("com.google.firebase:firebase-database:21.0.0")
     implementation("androidx.gridlayout:gridlayout:1.0.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.activity:activity:1.9.3")
-    implementation("com.google.firebase:firebase-firestore-ktx:25.1.1")
+    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("com.google.firebase:firebase-firestore-ktx:25.1.3")
 
         //ROOMDATABASE DEPENDENCIES
     val room_version = "2.6.1"
@@ -83,16 +83,16 @@ dependencies {
 
     // optional - Paging 3 Integration
     implementation("androidx.room:room-paging:$room_version")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
 
     // Coroutine Support (For background tasks)
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     implementation ("com.squareup.picasso:picasso:2.71828")
-    implementation("com.google.firebase:firebase-firestore:25.1.1")
+    implementation("com.google.firebase:firebase-firestore:25.1.3")
     implementation("com.google.firebase:firebase-storage-ktx:21.0.1")
     implementation("com.google.android.gms:play-services-cast-framework:22.0.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
